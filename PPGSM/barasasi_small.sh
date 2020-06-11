ee0bb694bd65a45c059bdcd87faeed53c30bae5f
PPGSM <anon@gmail.com>
2020-06-11 13:17:40 +0900

    initalization1

--- PPGSM/barasasi_small.sh
+++
@@ -1,6 +0,0 @@
-t=performance/barabasismall
-type=testnodetype
-info=testtypeinfo
-for i in $(seq 1 30); do
-	./homomorphic_graph $t/barabasiGraphSmall $t/$type $t/$info >> $t/heuristic_10node_$i
-done
--- PPGSM/random_large.sh
+++
@@ -1,6 +0,0 @@
-t=performance/randomlarge
-type=testnodetype
-info=testtypeinfo
-for i in $(seq 1 30); do
-	./homomorphic_graph $t/randomGraphLarge $t/$type $t/$info >> $t/heuristic_10node_$i
-done
