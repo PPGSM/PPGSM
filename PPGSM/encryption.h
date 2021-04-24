e0d001db645805517f5b5bba0bc4d6c7a177adbb
Lee Dongwon <dongwonlee95@gmail.com>
2021-04-24 16:19:57 +0900

    21-04-24 update

--- PPGSM/encryption.h
+++
@@ -1,3 +0,0 @@
-LweSample* integerEncryption(int number, int bitsize, const TFheGateBootstrappingSecretKeySet* PK);
-int integerDecryption(LweSample* Input, int bitsize, const TFheGateBootstrappingSecretKeySet* PK);
-
